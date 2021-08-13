fb575a46669d81c1f2f67a9c519864cc2c645c7b
Amit Maindola <amitkm.knp2019@gmail.com>
2021-08-13 20:38:07 +0530

    Change Port

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ app.use(express.static("assets"));
 app.get("/",function (req, res) {
     res.sendFile(__dirname+"/index.html");
 })
-app.listen(3000||process.env,function () {
+app.listen(3000||process.env.PORT,function () {
     console.log("The server has been started at port 3000");
 })
